bd095dd254004123c6fa3ea4ae41c9637d945cd1
modeverv <modeverv@gmail.com>
2013-03-03 18:58:02 +0900

    aaa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblog::Application.config.secret_key_base = '7a4f645ddfc507baca2d4777e6f7540bc8716976a5e4f70eefa0058d2931dbc25c5d5c98233ea795ce746d4dee66e6fd68fc0610e320b690db0d61e26e59c64f'
